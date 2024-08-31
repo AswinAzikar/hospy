89b2ae3dfe92faa8009a0f65f49a4bd2ff9711e0
AswinAzikar <azwinaswin8@gmail.com>
2024-08-31 18:21:01 +0530

    "Added PageView and navigation logic to BottomNavigationBar, updated StylishBottomBar configuration"

--- lib/bottom_navigation/bottom_navbar.dart
+++ lib/bottom_navigation/bottom_navbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hospy/constants/color_const.dart';
+import 'package:hospy/home_screen/home_screen.dart';
 import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 
 class BottomNavigationBar extends StatefulWidget {
@@ -10,11 +11,39 @@ class BottomNavigationBar extends StatefulWidget {
 }
 
 class _BottomNavigationBarState extends State<BottomNavigationBar> {
+  int selectedIndex = 0;
+  final PageController _pageController = PageController();
   @override
   Widget build(BuildContext context) {
-    return StylishBottomBar(
-        items: const [],
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        children: const <Widget>[
+          HomeScreen(),
+        ],
+      ),
+      bottomNavigationBar: StylishBottomBar(
+        items: [
+          BottomBarItem(
+              icon: const Icon(Icons.home_outlined), title: const Text('Home'))
+        ],
         option:
-            DotBarOptions(gradient: primaryGradience, dotStyle: DotStyle.tile));
+            DotBarOptions(dotStyle: DotStyle.tile, gradient: primaryGradience),
+            fabLocation: StylishBarFabLocation.end,
+            hasNotch: true,
+            currentIndex:selectedIndex ,
+            onTap: (index){
+              setState(() {
+                selectedIndex=index;
+                _pageController.jumpToPage(index);
+              });
+            },
+      ),
+    );
   }
 }
